9b8379c979e3c95b0fd1f29f2893c0ea56994961
Shiham123 <s36651786@gmail.com>
2023-09-19 12:33:22 +0600

    tailwind and scss are added for style purpose

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import './css/app.scss';
+
 function App() {
-  return <>this vite project</>;
+  return (
+    <>
+      <h1>this tailwind css checking</h1>
+      <h2 className="text-5xl font-bold text-white bg-black">
+        this tailwind css checking
+      </h2>
+    </>
+  );
 }
 
 export default App;
